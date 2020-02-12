bcb2148dd9757c4007c20e5867f1befc59bb8d34
Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>
2020-02-12 21:09:25 +0100

    feat(api): relaunch a workflow
    Signed-off-by: Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>

--- director/api/__init__.py
+++ director/api/__init__.py
@@ -5,6 +5,11 @@ from flask import jsonify, Blueprint
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 
 
+@api_bp.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": error.description}), 404
+
+
 @api_bp.route("/ping")
 def ping():
     return jsonify({"message": "pong"})
--- director/api/workflows.py
+++ director/api/workflows.py
@@ -1,11 +1,40 @@
 from flask import current_app as app
-from flask import jsonify, request
+from flask import abort, jsonify, request
 
 from director.api import api_bp
 from director.builder import WorkflowBuilder
 from director.exceptions import WorkflowNotFound
 from director.extensions import cel_workflows, schema
 from director.models.workflows import Workflow
+
+
+def _get_workflow(workflow_id):
+    workflow = Workflow.query.filter_by(id=workflow_id).first()
+    if not workflow:
+        abort(404, f"Workflow {workflow_id} not found")
+    return workflow
+
+
+def _execute_workflow(project, name, payload={}):
+    fullname = f"{project}.{name}"
+
+    # Check if the workflow exists
+    try:
+        cel_workflows.get_by_name(fullname)
+    except WorkflowNotFound:
+        abort(404, f"Workflow {fullname} not found")
+
+    # Create the workflow in DB
+    obj = Workflow(project=project, name=name, payload=payload)
+    obj.save()
+
+    # Build the workflow and execute it
+    data = obj.to_dict()
+    workflow = WorkflowBuilder(obj.id)
+    workflow.run()
+
+    app.logger.info(f"Workflow sent : {workflow.canvas}")
+    return obj.to_dict(), workflow
 
 
 @api_bp.route("/workflows", methods=["POST"])
@@ -21,27 +50,19 @@ from director.models.workflows import Workflow
     }
 )
 def create_workflow():
-    data = request.get_json()
-    project = data["project"]
-    name = data["name"]
-    fullname = f"{project}.{name}"
+    project, name, payload = (
+        request.get_json()["project"],
+        request.get_json()["name"],
+        request.get_json()["payload"],
+    )
+    data, _ = _execute_workflow(project, name, payload)
+    return jsonify(data), 201
 
-    # Check if the workflow exists
-    try:
-        cel_workflows.get_by_name(fullname)
-    except WorkflowNotFound:
-        return jsonify({"error": f"Workflow {fullname} not found"}), 404
 
-    # Create the workflow in DB
-    obj = Workflow(project=project, name=name, payload=data["payload"])
-    obj.save()
-
-    # Build the workflow and execute it
-    data = obj.to_dict()
-    workflow = WorkflowBuilder(obj.id)
-    workflow.run()
-
-    app.logger.info(f"Workflow ready : {workflow.canvas}")
+@api_bp.route("/workflows/<workflow_id>/relaunch", methods=["POST"])
+def relaunch_workflow(workflow_id):
+    obj = _get_workflow(workflow_id)
+    data, _ = _execute_workflow(obj.project, obj.name, obj.payload)
     return jsonify(data), 201
 
 
@@ -53,12 +74,9 @@ def list_workflows():
 
 @api_bp.route("/workflows/<workflow_id>")
 def get_workflow(workflow_id):
-    workflow = Workflow.query.filter_by(id=workflow_id).first()
-    if not workflow:
-        return jsonify({"error": f"Workflow {workflow_id} not found"}), 404
-
+    workflow = _get_workflow(workflow_id)
     tasks = [t.to_dict() for t in workflow.tasks]
+
     resp = workflow.to_dict()
     resp.update({"tasks": tasks})
-
     return jsonify(resp)
