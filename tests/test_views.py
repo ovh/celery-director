c492f9157eb81034bd5d627fbb207b21e6b34dc5
Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>
2020-05-05 20:50:17 +0200

    fix(error): properly handle the 404 errors
    Signed-off-by: Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>

---
+++ tests/test_views.py
@@ -0,0 +1,30 @@
+def test_view(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert not resp.is_json
+    assert resp.mimetype == "text/html"
+    assert "<title>Celery Director</title>" in str(resp.data)
+
+
+def test_404_view(client):
+    resp = client.get("/notfound")
+    assert resp.status_code == 404
+    assert not resp.is_json
+    assert resp.mimetype == "text/html"
+    assert "<title>404 Not Found</title>" in str(resp.data)
+
+
+def test_api(client):
+    resp = client.get("/api/ping")
+    assert resp.status_code == 200
+    assert resp.is_json
+    assert resp.mimetype == "application/json"
+    assert resp.json == {"message": "pong"}
+
+
+def test_404_api(client):
+    resp = client.get("/api/notfound")
+    assert resp.status_code == 404
+    assert resp.is_json
+    assert resp.mimetype == "application/json"
+    assert "404 Not Found" in resp.json["error"]
