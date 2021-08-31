57b5ba5587e2d0996d437a343bd6db2947fe2d14
Anthony Olea <anthony.olea@corp.ovh.com>
2021-08-31 12:14:59 +0200

    fix(migrations): add sqlite support for altering constraint, fix typo
    Signed-off-by: Anthony Olea <anthony.olea@corp.ovh.com>

---
+++ director/migrations/versions/46e4acde004e_add_cascade_in_task_model.py
@@ -0,0 +1,74 @@
+"""Add cascade in task model for workflow deletion
+
+Revision ID: 46e4acde004e
+Revises: 2ac615d6850b
+Create Date: 2021-07-16 11:54:27.946028
+
+"""
+from alembic import op
+from director.extensions import db
+
+
+# revision identifiers, used by Alembic.
+revision = "46e4acde004e"
+down_revision = "2ac615d6850b"
+branch_labels = None
+depends_on = None
+
+tables = db.metadata.tables
+
+
+def upgrade():
+    bind = op.get_bind()
+    if bind.engine.name == "sqlite":
+        with op.batch_alter_table("tasks", copy_from=tables["tasks"]) as batch_op:
+            batch_op.drop_constraint(
+                op.f("fk_tasks_workflow_id_workflows"), type_="foreignkey"
+            )
+            batch_op.create_foreign_key(
+                op.f("fk_tasks_workflow_id_workflows"),
+                "workflows",
+                ["workflow_id"],
+                ["id"],
+                ondelete="cascade",
+            )
+        return
+
+    op.drop_constraint(
+        op.f("fk_tasks_workflow_id_workflows"), "tasks", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        op.f("fk_tasks_workflow_id_workflows"),
+        "tasks",
+        "workflows",
+        ["workflow_id"],
+        ["id"],
+        ondelete="cascade",
+    )
+
+
+def downgrade():
+    bind = op.get_bind()
+    if bind.engine.name == "sqlite":
+        with op.batch_alter_table("tasks", copy_from=tables["tasks"]) as batch_op:
+            batch_op.drop_constraint(
+                op.f("fk_tasks_workflow_id_workflows"), type_="foreignkey"
+            )
+            batch_op.create_foreign_key(
+                op.f("fk_tasks_workflow_id_workflows"),
+                "workflows",
+                ["workflow_id"],
+                ["id"],
+            )
+        return
+
+    op.drop_constraint(
+        op.f("fk_tasks_workflow_id_workflows"), "tasks", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        op.f("fk_tasks_workflow_id_workflows"),
+        "tasks",
+        "workflows",
+        ["workflow_id"],
+        ["id"],
+    )
