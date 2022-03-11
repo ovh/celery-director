564934fded5ea2272a913abdb75296ba58c585ef
CodePint <geddie.code@gmail.com>
2022-03-11 00:06:32 +0000

    adding test_load for workflow formats

---
+++ tests/test_load.py
@@ -0,0 +1,13 @@
+import pytest
+
+from director.extensions import CeleryWorkflow
+
+
+@pytest.mark.parametrize("fmt", [('yml'), ('json')])
+def test_load_workflow_formats(app, fmt):
+    app.config["WORKFLOW_FORMAT"] = fmt
+    name = f"example.{fmt.upper()}_WORKFLOW"
+
+    cel_workflows = CeleryWorkflow()
+    cel_workflows.init_app(app)
+    cel_workflows.get_by_name(name)
